c2bcf9062b7c3a24659d915b8f272c1931a5802f
Nick Novitski <github@nicknovitski.com>
2024-10-28 08:07:53 -0700

    Preserve stdout from shellhooks
    Also only evaluate the shell once, instead of three times.
    
    Type: feature

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     packages = eachSystem ({pkgs, ...}: {
       default = pkgs.writeShellApplication {
         name = "nix-develop-gha";
-        runtimeInputs = [pkgs.gnugrep pkgs.openssl.bin pkgs.coreutils pkgs.gnused];
+        runtimeInputs = [pkgs.gnugrep pkgs.openssl.bin pkgs.coreutils];
         text = builtins.readFile ./nix-develop-gha.sh;
       };
     });
--- nix-develop-gha.sh
+++ nix-develop-gha.sh
@@ -5,27 +5,58 @@ set -euo pipefail
 # Read the arguments into an array, so they can be added correctly as flags
 IFS=" " read -r -a arguments <<<"${@:-./#default}"
 
-with_nix_develop() {
-	delimiter=$(openssl rand -base64 18)
-	nix develop "${arguments[@]}" --command bash -c "echo $delimiter; $*" | sed -n "\|$delimiter|,\$p" | tail -n +2
-}
-
-with_nix_develop true # Exit immediately if build fails
-
 contains() {
 	grep "$1" --silent <<<"$2"
 }
 
-# Add all environment variables except for PATH to GITHUB_ENV.
+envOutput=
+
+# Iterate over the output of `env -0` in the shell environment
 while IFS='=' read -r -d '' n v; do
-	if [ "$n" == "PATH" ]; then
+	# If this variable is empty then we're in the first loop, and $n is the
+	# output of the shellHook _before_ `env` is run.
+	if ! [ "$envOutput" ]; then
+		envOutput=1
+		if [ "$v" ]; then
+			# There was one or more equals signs in the hook output
+			printf "%s=%s" "$n" "$v"
+		else
+			printf "%s" "$n"
+		fi
 		continue
 	fi
-	# Skip if the variable is already in the environment with the same
+	if [ "$n" == "PATH" ]; then
+		# Read PATH elements into an array
+		IFS=":" read -r -a nix_path_array <<<"$v"
+		# Iterate over PATH elements in reverse.
+		#
+		# Why in reverse?  Appending a directory to $GITHUB_PATH causes
+		# that directory to be _prepended_ to $PATH in subsequent
+		# steps, so if we append in first-to-last order, the result
+		# will be in last-to-first order.  Order in PATH elements is
+		# significant, since it determines lookup order, thus we
+		# preserve their order by reversing them before they are
+		# reversed again.
+		for ((i = ${#nix_path_array[@]} - 1; i >= 0; i--)); do
+			nix_path_entry="${nix_path_array[$i]}"
+			if contains "$nix_path_entry" "$PATH"; then
+				continue
+			fi
+			if ! [ -d "$nix_path_entry" ]; then
+				continue
+			fi
+			echo "$nix_path_entry" >>"${GITHUB_PATH:-/dev/stderr}"
+		done
+		continue
+	fi
+	# Skip if the variable is already in the host environment with the same
 	# value (treating unset and the empty string as identical states)
 	if [ "${!n:-}" == "$v" ]; then
 		continue
 	fi
+	# Otherwise, echo name=value to $GITHUB_ENV
+	# Ref https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-an-environment-variable
+	# Use a random string as a heredoc delimiter for multi-line strings
 	if (("$(wc -l <<<"$v")" > 1)); then
 		delimiter=$(openssl rand -base64 18)
 		if contains "$delimiter" "$v"; then
@@ -35,26 +66,6 @@ while IFS='=' read -r -d '' n v; do
 		printf "%s<<%s\n%s%s\n" "$n" "$delimiter" "$v" "$delimiter" >>"${GITHUB_ENV:-/dev/stderr}"
 		continue
 	fi
+	# Add all environment variables except for PATH to GITHUB_ENV.
 	printf "%s=%s\n" "$n" "$v" >>"${GITHUB_ENV:-/dev/stderr}"
-done < <(with_nix_develop "env -0")
-
-# Read the nix environment's $PATH into an array
-IFS=":" read -r -a nix_path_array <<<"$(with_nix_develop "echo \$PATH")"
-
-# Iterate over the PATH array in reverse
-#
-# Why in reverse?  Appending a directory to $GITHUB_PATH causes that directory
-# to be _prepended_ to $PATH in subsequent steps, so if we append in
-# first-to-last order, the result will be in last-to-first order.  Order in
-# PATH elements is significant, since it determines lookup order, thus we
-# preserve their order by reversing them before they are reversed again.
-for ((i = ${#nix_path_array[@]} - 1; i >= 0; i--)); do
-	nix_path_entry="${nix_path_array[$i]}"
-	if contains "$nix_path_entry" "$PATH"; then
-		continue
-	fi
-	if ! [ -d "$nix_path_entry" ]; then
-		continue
-	fi
-	echo "$nix_path_entry" >>"${GITHUB_PATH:-/dev/stderr}"
-done
+done < <(nix develop "${arguments[@]}" --command bash -c "echo -ne '\0'; env -0")
